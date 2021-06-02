b5bed5514d4540940f0d88ef50459cce7f74adee
Enric Cervera <ecervera@uji.es>
2021-06-02 16:45:50 +0200

    Test images from front camera

---
+++ point_front.py
@@ -0,0 +1,48 @@
+from model.vision import DGCVision
+
+import cv2
+import numpy as np
+import sys
+
+COLORS = [(255,0,255),(255,255,0),(255,0,0),(0,255,0),(0,0,255)]
+color = 0
+
+# python point_front.py imgs/front/0060.png imgs/front/0001.png 
+# python point_front.py imgs/front/0095.png imgs/front/0060.png 
+# python point_front.py imgs/front/0150.png imgs/front/0095.png
+
+img1 = cv2.cvtColor(cv2.imread(sys.argv[1], cv2.IMREAD_COLOR), cv2.COLOR_BGR2RGB)
+img2 = cv2.cvtColor(cv2.imread(sys.argv[2], cv2.IMREAD_COLOR), cv2.COLOR_BGR2RGB)
+img1 = cv2.resize(img1, (640,360,))
+img2 = cv2.resize(img2, (640,360,))
+img = cv2.cvtColor(np.hstack((img1,img2,)), cv2.COLOR_RGB2BGR)
+
+dgc = DGCVision()
+dgc.predict(img1, img2)
+warp_img = dgc.warp()
+match_mask = dgc.getMatchabilityMask()
+flow = dgc.getFlow()
+
+cv2.namedWindow('Warped image')
+cv2.imshow('Warped image', cv2.cvtColor(warp_img, cv2.COLOR_RGB2BGR))
+
+cv2.namedWindow('DGC-Net')
+
+def onMouse(event, x, y, flags, param):
+    global color
+    if event == cv2.EVENT_LBUTTONDOWN and x < 640:
+       color = (color+1) % len(COLORS)
+       cv2.circle(img,(x,y),5,COLORS[color],-1)
+       xt, yt = dgc.target(x,y,0,0)
+       cv2.circle(img,(round(xt+640),round(yt)),5,COLORS[color],-1)
+
+cv2.setMouseCallback('DGC-Net', onMouse)
+
+while True:
+    cv2.imshow('DGC-Net', img)
+    k = cv2.waitKey(20) & 0xFF
+    if k == 27:
+        break
+    elif k == 32:
+        img = cv2.cvtColor(np.hstack((img1,img2,)),cv2.COLOR_RGB2BGR)
+
